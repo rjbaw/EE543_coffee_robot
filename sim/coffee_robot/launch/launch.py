c0a3363ed2da178b0e9d9c2fd7d7cde83bd3c1ba
rjbaw <raveeroj.b@rjbl.dev>
2024-06-06 22:13:59 -0400

    change bridge to yaml file

--- sim/coffee_robot/launch/launch.py
+++ sim/coffee_robot/launch/launch.py
@@ -10,6 +10,7 @@ def generate_launch_description():
     package_share_directory = get_package_share_directory("coffee_robot")
     world_path = os.path.join(package_share_directory, "models", "world.sdf")
     robot_path = os.path.join(package_share_directory, "models", "robot.sdf")
+    bridge_path = os.path.join(package_share_directory, "launch", "bridge.yaml")
 
     return LaunchDescription(
         [
@@ -27,63 +28,9 @@ def generate_launch_description():
                 package="ros_gz_bridge",
                 executable="parameter_bridge",
                 arguments=[
-                    "/clock@rosgraph_msgs/msg/Clock[gz.msgs.Clock"
-                    ],
-                output="screen",
-                ),
-            Node(
-                package="ros_gz_bridge",
-                executable="parameter_bridge",
-                arguments=[
-                    "/world/shapes/dynamic_pose/info@geometry_msgs/msg/PoseArray@gz.msgs.Pose_V"
-                    ],
-                output="screen",
-            ),
-            Node(
-                package="ros_gz_bridge",
-                executable="parameter_bridge",
-                arguments=[
-                    "/dd1@std_msgs/msg/Float64@ignition.msgs.Double"
-                    ],
-                output="screen",
-            ),
-            Node(
-                package="ros_gz_bridge",
-                executable="parameter_bridge",
-                arguments=[
-                    "/dtheta2@std_msgs/msg/Float64@ignition.msgs.Double"
-                    ],
-                output="screen",
-            ),
-            Node(
-                package="ros_gz_bridge",
-                executable="parameter_bridge",
-                arguments=[
-                    "/dtheta3@std_msgs/msg/Float64@ignition.msgs.Double"
-                    ],
-                output="screen",
-            ),
-            Node(
-                package="ros_gz_bridge",
-                executable="parameter_bridge",
-                arguments=[
-                    "/dtheta4@std_msgs/msg/Float64@ignition.msgs.Double"
-                    ],
-                output="screen",
-            ),
-            Node(
-                package="ros_gz_bridge",
-                executable="parameter_bridge",
-                arguments=[
-                    "/dtheta5@std_msgs/msg/Float64@ignition.msgs.Double"
-                    ],
-                output="screen",
-            ),
-            Node(
-                package="ros_gz_bridge",
-                executable="parameter_bridge",
-                arguments=[
-                    "/dtheta6@std_msgs/msg/Float64@ignition.msgs.Double"
+                    "--ros-args",
+                    "-p",
+                    "config_file:={}".format(bridge_path),
                     ],
                 output="screen",
             ),
